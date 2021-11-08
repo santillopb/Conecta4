3be8d27550cdda0c08636ab20603ce1b936cd060
Ester <ester.martinez@xarxatecactiva.com>
2021-11-08 21:04:16 +0100

    Estilos finales cuatro en raya

---
+++ paginaCentral.js
@@ -0,0 +1,12 @@
+const pvsp = document.querySelector("#pvsp");
+pvsp.addEventListener("click", () => {
+    location.href = "playervsplayer.html";
+});
+const pvscpu = document.querySelector("#pvscpu");
+pvscpu.addEventListener("click", () => {
+    location.href = "playervscpu.html";
+});
+const iconGH = document.querySelector("#icon");
+iconGH.addEventListener("click", () => {
+    location.href = "https://github.com/santillopb/Conecta4/";
+});
